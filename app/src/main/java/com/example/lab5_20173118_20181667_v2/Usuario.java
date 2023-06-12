40352f1746dc66a38092e90d312ef361e828284e
joelsilvera <joel.silvera@pucp.edu.pe>
2023-06-11 19:50:29 -0500

    arreglados

---
+++ app/src/main/java/com/example/lab5_20173118_20181667_v2/Usuario.java
@@ -0,0 +1,40 @@
+package com.example.lab5_20173118_20181667_v2;
+
+public class Usuario {
+    private String email;
+    private String name;
+    private String password;
+    private String telefono;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
